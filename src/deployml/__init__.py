424b7bd34f52ac56def3eeed79e7ee10c503804c
VenkatachalamSubramanianPeriyaSubbu <venkatachalam.sps@gmail.com>
2025-11-25 15:45:44 -0800

    python api for auto-teardown

--- src/deployml/__init__.py
+++ src/deployml/__init__.py
@@ -4,6 +4,9 @@ from .notebook import deploy, load, DeploymentStack, ServiceURLs
 # Import diagnostics for easy access
 from .diagnostics import run_doctor, check_system, DeployMLDoctor
 
+# Import Python API for teardown management
+from .api import get_teardown_status, update_teardown_schedule, cancel_teardown
+
 __all__ = [
     'deploy', 
     'load', 
@@ -11,5 +14,8 @@ __all__ = [
     'ServiceURLs',
     'run_doctor',
     'check_system', 
-    'DeployMLDoctor'
+    'DeployMLDoctor',
+    'get_teardown_status',
+    'update_teardown_schedule',
+    'cancel_teardown'
 ]
